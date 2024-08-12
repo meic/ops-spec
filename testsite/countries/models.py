7fdcf9ea0052776ddbeef07d80a33d1eee91e87f
Meic Williams <mwilliams@digitalcontrolroom.com>
2024-08-12 19:29:07 +0100

    Add medals table

--- testsite/countries/models.py
+++ testsite/countries/models.py
@@ -22,3 +22,21 @@ class Region(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Medals(models.Model):
+    TYPES = [
+        ("go", "Gold"),
+        ("si", "Silver"),
+        ("br", "Bronze"),
+    ]
+
+    type = models.CharField(
+        max_length=2,
+        choices=TYPES,
+    )
+    count = models.IntegerField()
+
+    country = models.ForeignKey(
+        "Country", on_delete=models.CASCADE, related_name="medals"
+    )
