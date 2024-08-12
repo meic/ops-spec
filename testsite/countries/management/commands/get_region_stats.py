6386d36a2a1c67c964aa3d756ee682cda572432f
Meic Williams <mwilliams@digitalcontrolroom.com>
2024-08-12 17:26:04 +0100

    new mgmt commands

---
+++ testsite/countries/management/commands/get_region_stats.py
@@ -0,0 +1,35 @@
+import csv
+
+from django.core.management.base import BaseCommand
+from django.db.models import Count, Sum
+
+from countries.models import Region
+
+
+class Command(BaseCommand):
+    help = "Exports stats on region."
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--output-file",
+            default="stats",
+            help="Filename for output file.",
+        )
+
+    def handle(self, *args, **options):
+        regions = Region.objects.annotate(
+            num_countries=Count("countries"),
+            total_population=Sum("countries__population"),
+        )
+
+        headings = ["Name", "Number Countries", "Total Population"]
+
+        data = [
+            [region.name, region.num_countries, region.total_population]
+            for region in regions
+        ]
+
+        with open(options["output_file"], "w") as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(headings)
+            writer.writerows(data)
---
+++ testsite/countries/management/commands/update_populations.py
@@ -0,0 +1,29 @@
+import csv
+
+from django.core.management.base import BaseCommand
+
+from countries.models import Country
+
+
+class Command(BaseCommand):
+    help = "Updates Country populations"
+
+    REQUIRED_HEADERS = ["alpha3Code", "population"]
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "input_file",
+            help="Filename for input file.",
+        )
+
+    def handle(self, *args, **options):
+        with open(options["input_file"]) as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                headers = (key for key in row.keys())
+                for header in self.REQUIRED_HEADERS:
+                    if header not in headers:
+                        raise Exception(f"Column not found: {header}")
+                country = Country.objects.get(alpha3Code=row["alpha3Code"])
+                country.population = int(row["population"])
+                country.save()
